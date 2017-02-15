88ee775d0781d0e0629d35d19b8dfe38f7958a44
Sterling Michel <smichel@agero.com>
2017-02-15 05:05:59 -0500

    Update the logic to support the search

--- gistapi/gistapi.py
+++ gistapi/gistapi.py
@@ -13,19 +13,33 @@ import requests
 from flask import Flask, jsonify, json, request, Response, render_template, copy_current_request_context,  redirect, url_for
 from flask_cache import Cache
 import time
+import re
+import datastore.core
 
 # *The* app object
 app = Flask(__name__, static_path='/static')
 
 conf = {
   'CACHE_TYPE': 'simple',
-   'CACHE_NO_NULL_WARNING': True,
+  'CACHE_NO_NULL_WARNING': True
 }
 
 cache = Cache(app, config=conf)
+ds = datastore.DictDatastore() # in memory
 
 
-@app.route("/")
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Cache-Control'] = 'no-cache, no-store' #'public, max-age=0'
+    return response
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
@@ -34,21 +48,26 @@ def home():
     return render_template('home.html')
 
 @app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'error': error,
+            'message': 'Not Found: ' + request.url,
+    }
+    return render_template('404.html', message=message), 404
 
 @app.route("/ping")
 def ping():
     """Provide a static response to a simple GET request."""
     return "pong"
 
-@app.route('/api/v1/<username>/list')
-@cache.cached(timeout=50, key_prefix='gist_user_list')
+@app.route('/api/v1/gist/<username>/list', methods = ['GET'])
+#@cache.cached(timeout=10, key_prefix='gist_user_list')
 def gist_user_list(username):
    if username:
      result = gists_for_user(username)
    else:
-     result = {"Error": u"Invalid username provided"}
+     return not_found("Invalid username provided")
 
    js = json.dumps(result, indent=4, sort_keys=True)
 
@@ -71,26 +90,50 @@ def gists_for_user(username):
         the above URL for details of the expected structure.
     """
 
-    # build the url
-    gists_url = 'https://api.github.com/users/{username}/gists'.format(username=username)
+    rs = []
 
-    try:
-      response = requests.get(gists_url)
-    except requests.exceptions.Timeout as e:
-      time.sleep(1)
-      response = requests.get(gists_url)
-    except requests.exceptions.HTTPError as err:
-      response = type('lamdbaobject', (object,), {})()
-      response.ok = false
+    def loop(url=None):
+      # build the url
+      per_page = 2
+      page = 1
+      gists_url = url or 'https://api.github.com/users/{username}/gists?per_page={per_page}&page={page}'.format(per_page=per_page, page=page, username=username)
+      gists_url += "&client_id=%s&client_secret=%s" %("3cc58ae648e5bfd676cf", "f908a9ddcebdbc36d38c1fe902f98cb12d15c44c")
+
+      try:
+        response = requests.get(gists_url)
+      except requests.exceptions.Timeout as e:
+        time.sleep(1)
+        response = requests.get(url)
+      except requests.exceptions.HTTPError as err:
+        response = type('lamdbaobject', (object,), {})()
+        response.ok = false
       
+      if response.ok:
+        # grab the data response
+        rs.extend(response.json())
 
-    # BONUS: What failures could happen?
-    # BONUS: Paging? How does this work for users with tons of gists?
+        if response.headers.get('Link', None):
+          # parse the for the next url
+          links = response.headers['Link'].split(',')
+          
+          # extra the url
+          next_url = re.match('<(.*)>; rel="next"', links[0])
 
-    if response.ok:
-      return response.json()
-    else:
-      return {}
+          # BONUS: What about huge gists?
+          if next_url:
+            gists_url = next_url.groups()[0]
+            loop(gists_url)
+
+        else:
+          rs.extend(response.json())
+
+      else:
+        # BONUS: Handle invalid users?
+        return response.json()
+
+      return rs
+
+    return loop()
 
 @app.route("/api/v1/search", methods=['POST'])
 def search():
@@ -104,26 +147,50 @@ def search():
         object contains the list of matches along with a 'status' key
         indicating any failure conditions.
     """
-    post_data = request.get_json()
-    # BONUS: Validate the arguments?
 
+    post_data = request.get_json()
+
+    # BONUS: Validate the arguments?
+    if post_data:
+      if not('username' in post_data):
+        return jsonify({'Error': 'Invalid user is given'})
+
+      if not('pattern' in post_data):
+        return jsonify({'Error': 'Invalid pattern is given'})
+
+    else:
+      return jsonify({"Error": "'application/json' was not set for mine-type and accept"})
+ 
     username = post_data['username']
     pattern = post_data['pattern']
 
     result = {}
-    gists = gists_for_user(username)
-    # BONUS: Handle invalid users?
+   
+    # set the key
+    dsKey = datastore.Key(username)
 
+    # BONUS: Can we cache results in a datastore/db?
+    # validate above
+    if ds.contains(dsKey):
+       gists = ds.get(dsKey)
+    else:
+      gists = gists_for_user(username)
+
+      # store a key for the datastore
+      ds.put(dsKey, gists)
+
+    matches = []
     for gist in gists:
-        # REQUIRED: Fetch each gist and check for the pattern
-        # BONUS: What about huge gists?
-        # BONUS: Can we cache results in a datastore/db?
-        pass
+       # REQUIRED: Fetch each gist and check for the pattern
+       txt_version = str(gist)
+       search_gist = re.findall(pattern, txt_version, re.IGNORECASE)
+       if search_gist:
+         matches.append(gist)
 
     result['status'] = 'success'
     result['username'] = username
     result['pattern'] = pattern
-    result['matches'] = []
+    result['matches'] = matches
 
     return jsonify(result)
 
--- gistapi/static/js/gitapi.js
+++ gistapi/static/js/gitapi.js
@@ -5,28 +5,16 @@
 (function () {
     'use strict';
     angular
-            .module('GitApi', ['restangular', 'restangular-object-cache', 'ngStorage', 'ngRoute', 'ui.router', 'ngAnimate', 'blockUI'])
-            .config(['RestangularProvider', '$routeProvider', 'blockUIConfig', '$interpolateProvider',
-                function (RestangularProvider, $routeProvider, blockUIConfig, $interpolateProvider) {
-                    
-                    // handle the template tag because of jinja uses the {{ && }}
-                    $interpolateProvider.startSymbol('[[').endSymbol(']]');
+            .module('GitApi', ['restangular', 'restangular-object-cache', 'ngStorage', 'ngRoute', 'ui.router', 'ngAnimate', 'ngTouch', 'ui.grid'])
+            .config(['RestangularProvider', '$routeProvider', '$interpolateProvider', '$logProvider',
+                function (RestangularProvider, $routeProvider, $interpolateProvider, $logProvider) {
+
+                    // enable the loging to display
+                    $logProvider.debugEnabled(true);
 
                     // set the default angular route handlers
                     RestangularProvider.setBaseUrl('/api/v1');
-                    RestangularProvider.setDefaultHttpFields({cache: true});
-
-                    // Change the default overlay message
-                    blockUIConfig.message = 'Loading.....!';
-
-                    // Change the default delay to 100ms before the blocking is visible
-                    blockUIConfig.delay = 100;
-
-                    // Disable automatically blocking of the user interface
-                    blockUIConfig.autoBlock = false;
-
-                    // Disable clearing block whenever an exception has occurred
-                    blockUIConfig.resetOnException = false;
+                    RestangularProvider.setDefaultHttpFields({cache: false});
 
                     // Handle all the route at the client side
                     $routeProvider
@@ -39,44 +27,121 @@
                             });
 
                 }])
-            .service('GitApiService', ['Restangular,', 'RestangularObjectCache', '$sessionStorage', 'blockUI', function (Restangular, RestangularObjectCache, $sessionStorage, blockUI) {
-                    var self = this;
-                    RestangularObjectCache.track('employees');
+            .factory('GitApiService', ['$sessionStorage', 'Restangular', '$q', function ($sessionStorage, Restangular, $q) {
 
-                    self.getGistList = function (username) {
-                        blockUI.start("My custom message");
-                        console.log(username + RestangularObjectCache);
+                    var getGistList = function (username) {
+
+                        var deferred = $q.defer();
+
+                        Restangular
+                                .one('gist')
+                                .one(username)
+                                .getList('list')
+                                .then(function (data) {
+                                    deferred.resolve(data);
+                                });
+
+                        return deferred.promise;
                     };
 
-                    return self;
+                    return {
+                        getGistList: getGistList
+                    };
                 }])
-            .factory('GitApiFactory', ['GitApiService', '$sessionStorage', function (GitApiService, $sessionStorage) {
+            .service('GitApiFactory', ['$sessionStorage', 'GitApiService', function ($sessionStorage, GitApiService) {
                     function GitApi() {
-                        var self = this;
-
-                        self.getGistList = function (username) {
-
+                        this.getGistList = function (username) {
                             return GitApiService.getGistList(username);
                         };
                     }
 
                     return new GitApi();
                 }])
-            .controller('GitApiController', ['$scope', '$sessionStorage', 'GitApiFactory', '$log', function ($scope, $sessionStorage, GitApiFactory, $log) {
+            .run(['$rootScope', function ($rootScope) {
+                    $rootScope.safeApply = function (fn) {
+                        var phase = this.$root.$$phase;
+                        if (phase === '$apply' || phase === '$digest') {
+                            if (fn && (typeof (fn) === 'function')) {
+                                fn();
+                            }
+                        } else {
+                            this.$apply(fn);
+                        }
+                    };
+                }])
+            .controller('GitApiController', ['$scope', '$sessionStorage', 'GitApiFactory', '$log', 'uiGridConstants', function ($scope, $sessionStorage, GitApiFactory, $log, uiGridConstants) {
                     var self = this;
 
+//                    // gridAPI
+//                    $scope.gridApi = null;
+
+                    // binding the search field
+                    $scope.filterValue = "";
+
+                    // grab & set for user
+                    $scope.givenUser = "codeithuman";
+
                     // define a list of left menu
-                    $scope.leftMenuItems = ['ReadMe', 'Overview', 'List', 'Search', 'Export'];
+                    $scope.leftMenuItems = ['ReadMe', 'List'];
 
                     // get the first element as default
                     $scope.selleftMenuItem = $scope.leftMenuItems[0];
+
+                    // Store the object results
+                    $scope.gridOptions = {
+                        paginationPageSizes: [25, 50, 75],
+                        paginationPageSize: 25,
+                        onRegisterApi: function (gridApi) {
+                            $scope.gridApi = gridApi;
+                            $scope.gridApi.grid.registerRowsProcessor($scope.singleFilter, 200);
+                            $scope.safeApply();
+                        },
+                        columnDefs: [
+                            {name: 'id'},
+                            {name: 'name'},
+                            {name: 'created_at'}
+                        ],
+                        data: []
+                    };
+
+                    $scope.filter = function () {
+                        $scope.gridApi.grid.refresh();
+                    };
+
+                    // Adding a single filter
+                    $scope.singleFilter = function (renderableRows) {
+                        var matcher = new RegExp($scope.filterValue, 'i');
+
+                        renderableRows.forEach(function (row) {
+                            var match = false;
+                            ['id', 'name', 'created_at'].forEach(function (field) {
+                                if (row.entity[field].match(matcher)) {
+                                    match = true;
+                                }
+                            });
+                            if (!match) {
+                                row.visible = false;
+                            }
+                        });
+                        return renderableRows;
+                    };
 
                     // update select menu   
                     $scope.setLeftMenu = function (menu) {
                         $log.log("Menu: " + menu);
                         switch (menu) {
                             case 'List':
-                                GitApiFactory.getGistList('sterlingmichel');
+                                GitApiFactory
+                                        .getGistList($scope.givenUser)
+                                        .then(function (gists) {
+                                            angular.forEach(gists, function (gist, i) {
+                                                $scope.gridOptions.data.push({
+                                                    id: gist.id,
+                                                    name: gist.description,
+                                                    created_at: gist.created_at
+                                                });
+                                            });
+                                        });
                                 break;
                             default:
                                 break;
